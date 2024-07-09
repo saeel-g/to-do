66f1b91dd2f0082d668ed5c0379fb6148ba37d04
saeel-g <saeelg30@gmail.com>
2024-07-10 00:20:31 +0530

    Done POST GET

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for
 # from flask_sqlalchemy import SQLAlchemy
 import sqlite3
-from datetime import datetime
+from datetime import date
 
 app = Flask(__name__)
 
@@ -21,24 +21,31 @@ connect.close()
 @app.route('/', methods=["POST","GET"])
 def index():
     if request.method == 'POST':
-        content = request.form['name']
-        status = request.form['status']
-        create_on = datetime.now.strftime("%Y-%m-%d %H:%M:%S")
-
+        content = request.form["content"]
+        status = request.form.get("status", "0")
+        status = 1 if status == "on" else 0
+        created_on = date.today().strftime("%Y-%m-%d")
         with sqlite3.connect("database.db") as users:
             cursor = users.cursor()
-            cursor.execute("INSERT INTO TODO \ (content, create_on, status)VALUES (?,?,?)", (content, create_on, status))
+            cursor.execute("INSERT INTO TODO (content, created_on, status)VALUES (?,?,?)", (content, created_on, status))
             users.commit()
-        # return render_template("index.html")
-        return redirect(url_for('index'))
-    else:
-        with sqlite3.connect("database.db") as conn:
-            cursor = conn.cursor()
+        with sqlite3.connect("database.db") as users:
+            cursor = users.cursor()
             cursor.execute("SELECT * FROM TODO")
             todos = cursor.fetchall()
-
+        print("*************POST request sent******************")
         # return render_template("index.html")
-        return render_template("index.html", todos=todos)
+        return render_template("index.html",todos=todos)
+   
+    else:
+        print("*************GET request sent******************")
+        with sqlite3.connect("database.db") as users:
+            cursor = users.cursor()
+            cursor.execute("SELECT * FROM TODO")
+            todos = cursor.fetchall()
+        # return render_template("index.html")
+        print("*************GET request sent******************")
+        return render_template("index.html",todos=todos)
 
 if __name__ == "__main__":
     app.run(debug=True)
